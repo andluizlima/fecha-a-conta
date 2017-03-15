37c900df30495447301699e22b63aa4caa031cb5
andluizlima <rodrigues_andre@id.uff.br>
2017-03-15 00:19:01 -0300

    Celula

---
+++ Fecha A Conta/AbertasViewController.swift
@@ -0,0 +1,94 @@
+//
+//  AbertasViewController.swift
+//  Fecha A Conta
+//
+//  Created by André Luiz Rodrigues on 14/03/17.
+//  Copyright © 2017 Andre Luiz Rodrigues. All rights reserved.
+//
+
+import UIKit
+
+class AbertasViewController: UITableViewController {
+    
+    var mesasAbertas: [Mesa]!
+    var clientesMesasAbertas: [[Cliente]]!
+    var pedidosMesasAbertas: [[Pedido]]!
+    var pedidosDosClientesAbertos: [[Pedido]]!
+    var clientesDosPedidosAbertos: [[Cliente]]!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return mesasAbertas.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath) as! CelulaMesasAbertas
+        
+        cell.nomeLocal.text = mesasAbertas[indexPath.row].local
+        cell.pessoasLocal.text = String(clientesMesasAbertas[indexPath.row].count) + " pessoas no local."
+        cell.totalLocal.text = "R$" + String(format: "%.2f", mesasAbertas[indexPath.row].total)
+
+        // Configure the cell...
+
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Fecha A Conta/CelulaMesasAbertas.swift
@@ -0,0 +1,16 @@
+//
+//  CelulaMesasAbertas.swift
+//  Fecha A Conta
+//
+//  Created by André Luiz Rodrigues on 14/03/17.
+//  Copyright © 2017 Andre Luiz Rodrigues. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CelulaMesasAbertas: UITableViewCell {
+    @IBOutlet weak var pessoasLocal: UILabel!
+    @IBOutlet weak var totalLocal: UILabel!
+    @IBOutlet weak var nomeLocal: UILabel!
+}
